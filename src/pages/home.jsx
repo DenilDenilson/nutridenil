148be5f3b207e2f95a7dbd563d0e99de983998f8
DenilDenilson <jordanpreseleccion@gmail.com>
2023-04-05 22:34:25 -0500

    Continuing trying

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,6 +3,11 @@ import MyImage from "@/components/MyImage";
 import NavBar from "@/components/NavBar";
 import SearchInput from "@/components/SearchInput";
 
+// require("dotenv").config();
+
+// console.log(process.env.NODE_ENV);
+// console.log("Hola");
+
 export default function Home() {
   return (
     <div className="h-screen bg-[#FBFDFE]">
