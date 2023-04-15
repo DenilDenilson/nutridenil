463d4cb13686256b578b2886005fd2d7838a0cc8
DenilDenilson <jordanpreseleccion@gmail.com>
2023-04-14 22:05:29 -0500

    createContext

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,17 @@
 import "@/styles/globals.css";
+import { createContext, useState } from "react";
 // import localFont from "next/font/local";
 
 // const myFont = localFont({ src: "./rotaFont/Rota-ExtraBold.otf" });
 
+const dataContext = createContext(null);
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [data, setData] = useState({});
+
+  return (
+    <dataContext.Provider value={data}>
+      <Component {...pageProps} />;
+    </dataContext.Provider>
+  );
 }
