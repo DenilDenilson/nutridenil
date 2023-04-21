d722315f6a64e0fd37216cc8b6af622f3e7ed77f
DenilDenilson <jordanpreseleccion@gmail.com>
2023-04-21 00:49:57 -0500

    Understanding better Next Js and router

--- src/pages/id/[id].jsx
+++ src/pages/id/[id].jsx
@@ -6,36 +6,44 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 export default function FoodId() {
-  const [plato, setPlato] = useState({});
+  const [plato, setPlato] = useState(["No se conectó a la API"]);
 
   const router = useRouter();
   const pathSegments = router.asPath.split("/");
   const id = pathSegments[pathSegments.length - 1];
+  // if (id === "[id]") {
+  //   id = 0;
+  // }
 
   useEffect(() => {
     function fetchData() {
-      // fetch(
-      //   `https://api.spoonacular.com/recipes/${id}/nutritionWidget.json?apiKey=7d5f2d4e10c14a04ae9dbd0a957f70ce`
-      // )
-      fetch(
-        "https://api.spoonacular.com/recipes/655055/nutritionWidget.json?apiKey=7d5f2d4e10c14a04ae9dbd0a957f70ce"
-      )
-        // .then((data) => data.json())
-        // .then((data) => {
-        //   if (data && data.calories) {
-        //     // comprueba si hay datos y que sean JSON válidos
-        //     setPlato(data);
-        //   }
-        // })
-        .then((res) => res.json())
-        .then((data) => console.log(data))
-        .then((data) => setPlato(data))
-        .then((data) => console.log(data));
-      // .catch((error) => console.log(error)); // maneja los errores de la API
+      if (id === "[id]") {
+      } else {
+        fetch(
+          `https://api.spoonacular.com/recipes/${id}/nutritionWidget.json?apiKey=7d5f2d4e10c14a04ae9dbd0a957f70ce`
+        )
+          .then((res) => res.json())
+          .then((data) => setPlato(data))
+          .catch((err) => console.log(err));
+      }
     }
     fetchData();
-    // console.log(plato);
   }, [id]);
+
+  // useEffect(() => {
+  //   function fetchData() {
+  //     fetch(
+  //       "https://api.spoonacular.com/recipes/655055/nutritionWidget.json?apiKey=7d5f2d4e10c14a04ae9dbd0a957f70ce"
+  //     )
+  //       .then((res) => res.json())
+  //       .then((data) => console.log(data))
+  //       .then((data) => setPlato(data))
+  //       .then((data) => console.log(data));
+  //     // .catch((error) => console.log(error)); // maneja los errores de la API
+  //   }
+  //   fetchData();
+  //   // console.log(plato);
+  // }, [id]);
 
   return (
     <div className="h-screen bg-gray-bg ">
@@ -43,7 +51,9 @@ export default function FoodId() {
         <div className="px-4 w-full z-30 fixed bg-gray-bg flex flex-col py-2 gap-4">
           <NavBar />
         </div>
-        <FoodContainer allPlato={plato} />
+        {/* <FoodContainer allPlato={plato} /> */}
+        {<p className="absolute top-32">{plato.calories}</p>}
+        {<p className="absolute top-40">{id}</p>}
       </div>
       <BotBar />
     </div>
