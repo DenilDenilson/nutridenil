b34462e670693c457123ad75fec5f17aefd2de04
DenilDenilson <jordanpreseleccion@gmail.com>
2023-05-03 19:24:29 -0500

    Icons sun and moon react versions added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,13 @@
 import NutriDenil from "./NutriDenil";
+import IconSun from "./svg/IconSun";
 import Person from "./svg/Person";
 
 export default function NavBar() {
   return (
     <nav className="flex justify-between small-regular items-center">
       <NutriDenil />
-      <Person />
+      {/* <Person /> */}
+      <IconSun />
     </nav>
   );
 }
